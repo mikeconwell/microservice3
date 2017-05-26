8f2fdf6e1582081e9fa75d35fe90ef7cda4d483c
Mike Conwell <mike@mikeconwell.com>
2017-05-26 12:45:09 -0500

    OAK-197 Final working Eclipse / start.spring.io combination
    After a lot of false starts, was able to marry together a Spring Intialized project with Eclipse without build errors.   Thomas Preston's https://www.youtube.com/watch?v=pLUlYnqv32M got me past the hurdle.   I'm using this as the master, and will add GitFlow to the repository once I get underway so that I've got a working, albeit naked, master to restart from.

---
+++ src/main/java/com/mikeconwell/microservice3/Microservice3Application.java
@@ -0,0 +1,12 @@
+package com.mikeconwell.microservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice3Application.class, args);
+	}
+}
